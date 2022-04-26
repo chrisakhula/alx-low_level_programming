c7ecb12ddc3929a579b2cd5b32c412b304cc8df6
Cleophas <61723930+chrisakhula@users.noreply.github.com>
2022-04-26 10:58:53 +0300

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,25 +1,18 @@
-#include "main.h"
-#include <studio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
- * main - check the code for ALX students.
- *
- * Return: Always 0.
- */
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-int main(void)
-{
-	char s1[98] = "Hello";
-	char s2[] = "world!\n";
-	char *ptr;
-
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
-}
-
+#endif
